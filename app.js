bf1d70004f560f5fe336cfc5010746d6a3e1093e
“Sergio <sergiodatos3@gmail.com>
2023-11-14 22:24:33 -0300

    agregue imagenes de cert2 ificados dos

---
+++ app.js
@@ -0,0 +1,15 @@
+function mostrar() {
+document.getElementById('img').style.display = 'block'
+}
+
+function ocultar() {
+    document.getElementById('img').style.display = 'none'
+}
+
+function mostrar2() {
+    document.getElementById('img2').style.display = 'block'
+    }
+    
+    function ocultar2() {
+        document.getElementById('img2').style.display = 'none'
+    }
