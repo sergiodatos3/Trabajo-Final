78215e1e5aa00cac424c47792fd70428a70b1760
“Sergio <sergiodatos3@gmail.com>
2023-11-27 14:03:38 -0300

    movi menu de opciones

--- app.js
+++ app.js
@@ -13,3 +13,37 @@ function mostrar2() {
     function ocultar2() {
         document.getElementById('img2').style.display = 'none'
     }
+
+//menu lateral
+var menu_visible = false;
+let menu = document.getElementById ("nav");
+function mostrarOcultarMenu() {
+if (menu_visible==false) {// si esta oculto
+menu.style.display = "block"
+menu_visible = true
+}
+else{
+    menu.style.display = "none"
+    menu_visible = false
+}
+}
+
+
+//oculto el menu una vez que selecciono una opción
+let links = document.querySelectorAll ("nav a")
+for (var x = 0; x <links.length;x++) {
+    links[x].onclick = function() {
+        menu.style.display = "none"
+        menu_visible = false
+    }
+}
+
+
+//const $html = document.documentElement
+//$body = document.body
+//let touchpadmedio = getComputedStyle($html).getPropertyValue ("scroll-behavior")
+//let touchpadlento = getComputedStyle ($html).getPropertyValue ("")
+
+
+
+
